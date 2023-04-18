e8b41d99d47ad4161b97bf89253b50f1eb53c358
rui-rui-an <1405613785@qq.com>
2023-04-19 00:25:55 +0800

    测试

---
+++ src/views/Search.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <h1>This is 就哈哈伤害到收到货 about page</h1>
+    <div
+      sajkdhwqe="wqekljsadklsad" sajkdhaswqe="wqekljsadwqeklsad" sajkdhwsqe="wqekljsadkqswelsad"
+      sajkdahwsdqe="wqeksljsadklsad"
+      sajkdasdhwqe="wqekljsadklsad"
+    ></div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Home',
+  methods: {
+    aaa () {
+      console.log('aaa')
+    }
+  }
+}
+</script>
