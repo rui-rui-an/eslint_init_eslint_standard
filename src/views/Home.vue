b8c66be8c92eb1fd5215c44e02425b2a1f53512c
rui-rui-an <1405613785@qq.com>
2023-04-18 23:43:24 +0800

    测试一下

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,6 +13,13 @@ export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  methods: {
+    test () {
+      const a = 1111
+      console.log(a)
+      console.log(111111111111111)
+    }
   }
 }
 </script>
