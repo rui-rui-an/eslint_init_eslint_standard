77caa71e0bff9d32596aaffac51c7e1c902367b4
rui-rui-an <1405613785@qq.com>
2023-04-19 00:39:44 +0800

    测试

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -2,11 +2,18 @@
   <div class="about">
     <h1>This is 就哈哈伤害到收到货 about page</h1>
     <div
-      sajkdhwqe="wqekljsadklsad" sajkdhaswqe="wqekljsadwqeklsad" sajkdhwsqe="wqekljsadkqswelsad"
+      sajkdhwqe="wqekljsadklsad"
+      sajkdhaswqe="wqekljsadwqeklsad"
+      sajkdhwsqe="wqekljsadkqswelsad"
       sajkdahwsdqe="wqeksljsadklsad"
       sajkdasdhwqe="wqekljsadklsad"
     ></div>
-
+    <div wlqkjewqe="sakldjklwq">
+      <div class="child2"></div>
+      <div class="child1"></div>
+      <div class="childs3"></div>
+      <div class="child1234"></div>
+    </div>
   </div>
 </template>
 <script>
@@ -15,6 +22,9 @@ export default {
   methods: {
     aaa () {
       console.log('aaa')
+    },
+    fix () {
+      console.log(141545)
     }
   }
 }
